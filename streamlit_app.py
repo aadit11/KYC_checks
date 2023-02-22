1e3392d4805519934f90a5f53dabb2772c82ae32
aadit11 <aadit.kshirsagar@gmail.com>
2023-02-23 01:19:19 +0530

    image processing done....pdf testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,25 @@
 import streamlit as st
+import pytesseract
+import PyPDF2
+from PIL import Image
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+
 st.write("Hello World")
+
+uploaded_file = st.file_uploader("Choose a file")
+
+# image = Image.open(uploaded_file)
+
+# text = pytesseract.image_to_string(image)
+# st.write(text)
+
+
+pdf_reader = PyPDF2.PdfReader(uploaded_file)
+text = ''
+for page_num in range(len(pdf_reader.pages) ):
+    page = pdf_reader.pages[page_num]
+    text += page.extract_text()
+recognized_text = pytesseract.image_to_string(text)
+st.write(recognized_text)
