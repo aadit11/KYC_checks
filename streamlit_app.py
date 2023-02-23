58a228de80fd2fff539e5adb8d2964216679dfb5
sahil-gidwani <sahilgdwn@gmail.com>
2023-02-24 01:14:31 +0530

    pdf and img processing done

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,41 @@
 import streamlit as st
 import pytesseract
-import PyPDF2
+from pdf2image import convert_from_bytes
 from PIL import Image
+import io
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+poppler_path = r"C:\Users\SAHIL\Downloads\Release-23.01.0-0\poppler-23.01.0\Library\bin"
+
+st.subheader("Please upload a PDF or image file for text recognition!")
+
+accepted_file_extensions = ['pdf', 'png', 'jpg', 'jpeg']
+
+uploaded_file = st.file_uploader(
+    "Choose a file", type=accepted_file_extensions)
 
 
-st.write("Hello World")
-
-uploaded_file = st.file_uploader("Choose a file")
-
-# image = Image.open(uploaded_file)
-
-# text = pytesseract.image_to_string(image)
-# st.write(text)
+def process_image(image):
+    text = pytesseract.image_to_string(image)
+    st.write(text)
 
 
-pdf_reader = PyPDF2.PdfReader(uploaded_file)
-text = ''
-for page_num in range(len(pdf_reader.pages) ):
-    page = pdf_reader.pages[page_num]
-    text += page.extract_text()
-recognized_text = pytesseract.image_to_string(text)
-st.write(recognized_text)
+if uploaded_file:
+
+    file_extension = uploaded_file.name.split('.')[-1]
+    image_extensions = accepted_file_extensions[1:]
+
+    if file_extension in image_extensions:
+
+        image = Image.open(uploaded_file)
+        process_image(image)
+
+    else:
+
+        with io.BytesIO(uploaded_file.read()) as pdf_file:
+            images = convert_from_bytes(pdf_file.read(
+            ), poppler_path=poppler_path)
+
+        for i, img in enumerate(images):
+            # st.image(img, caption=f'PDF page {i+1}')
+            process_image(img)
