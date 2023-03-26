8663f90ddfff92aec53bd6cb242f4121f763395d
sahil-gidwani <sahilgdwn@gmail.com>
2023-03-26 22:43:22 +0530

    report generation done

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,23 +24,24 @@ uploaded_file = st.file_uploader(
     "Choose a file", type=accepted_file_extensions)
 
 
-def generate_bank_pdf(bank_line, account_name_line, account_no_line, address_line, branch_line, nomination_required_line, balance_line):
+def generate_pdf(info):
 
     pdf = FPDF()
     pdf.add_page()
 
     # Add header image
-    pdf.image('C:\IntellectProject\Images\header.jpg', x=0, y=0, w=210)
+    pdf.image(r'images\header.jpg', x=0, y=0, w=210)
 
     # Add footer image
-    pdf.image('C:\IntellectProject\Images\footer.jpg', x=160, y=272, w=50)
+    pdf.image(r'images\footer.jpg', x=160, y=272, w=50)
 
     pdf.set_font('Arial', 'B', 16)
-    pdf.set_xy(50, 78)
-    pdf.multi_cell(0, 10, bank_line + "\n" + account_name_line + "\n" + account_no_line + "\n" +
-                   address_line + "\n" + branch_line + "\n" + nomination_required_line + "\n" + balance_line)
 
-    filename = "bank_statement"+datetime.now().strftime("%d-%m-%Y,%H:%M:%S")+".pdf"
+    pdf.set_xy(0, 60)
+    for key, value in info.items():
+        pdf.cell(0, 10, txt=key + ": " + value, ln=1, align="C")
+
+    filename = "report/"+datetime.now().strftime("%d-%m-%Y,%H-%M-%S")+".pdf"
     st.download_button("Download Report", data=pdf.output(
         dest='S').encode('latin-1'), file_name=filename)
     st.success('Report generated successfully!', icon="✅")
@@ -65,7 +66,7 @@ def process_image(image):
     elif 'permanent account number' in text.lower():
         process_pan(threshold)
 
-    elif 'passport number' in text.lower():
+    elif 'passport' in text.lower():
         process_passport(threshold)
 
     else:
@@ -78,7 +79,7 @@ def process_bank_statement(threshold):
 
     # Split the text into lines and store them in a list
     lines = text.split('\n')
-    st.write(lines)
+    # st.write(lines)
 
     # Parse the extracted text to find the name and PAN card number using regular expressions
     bank_names = ['ICICI', 'HDFC', 'SBI', 'State Bank of India',
@@ -109,15 +110,48 @@ def process_bank_statement(threshold):
     if bank_match:
         bank = bank_match.group(1)
     else:
-        bank = "Bank name not found"
+        bank = "Not found!"
 
-    st.write("Bank:", bank)
-    st.write("Account Number:", account_number_match.group(1))
-    st.write("Account Name:", account_name_match.group(1))
-    st.write("Address:", address_match.group(1))
-    st.write("Branch:", branch_match.group(1))
-    st.write("Nomination:", nomination_match.group(1))
-    st.write("Balance:", balance_match[-1])
+    if account_name_match:
+        account_name = account_name_match.group(1)
+    else:
+        account_name = "Not found!"
+
+    if account_number_match:
+        account_number = account_number_match.group(1)
+    else:
+        account_number = "Not found!"
+
+    if address_match:
+        address = address_match.group(1)
+    else:
+        address = "Not found!"
+
+    if branch_match:
+        branch = branch_match.group(1)
+    else:
+        branch = "Not found!"
+
+    if nomination_match:
+        nomination = nomination_match.group(1)
+    else:
+        nomination = "Not found!"
+
+    if balance_match:
+        balance = balance_match[-1]
+    else:
+        balance = "Not found!"
+
+    generate_pdf({"Bank": bank, "Account Name": account_name, "Account Number": account_number,
+                 "Address": address, "Branch": branch, "Nomination": nomination, "Balance": balance})
+
+    # st.write("Bank:", bank)
+    # st.write("Account Name:", account_name)
+    # st.write("Account Number:", account_number)
+    # st.write("Address:", address)
+    # st.write("Branch:", branch)
+    # st.write("Nomination:", nomination)
+    # st.write("Balance:", balance)
 
 
 def process_pan(threshold):
@@ -131,7 +165,7 @@ def process_pan(threshold):
 
     # Split the text into lines and store them in a list
     lines = text.split('\n')
-    st.write(lines)
+    # st.write(lines)
 
     # Parse the extracted text to find the name and PAN card number using regular expressions
     name_pattern = re.compile(
@@ -141,47 +175,45 @@ def process_pan(threshold):
     fname_pattern = re.compile(
         r"Father's\s+Name\s*:?\s*(\w+\s+\w+\s+\w+)", re.IGNORECASE)
     dob_pattern = re.compile(
-        r"Date\s+of\s+Birth\s+\n(\d{2}/\d{2}/\d{4})", re.IGNORECASE)
-
-
+        r"(\d{2}/\d{2}/\d{4})", re.IGNORECASE)
 
     # Search for matches using the regular expressions
     name_match = name_pattern.search(text)
     pan_match = pan_pattern.search(text)
-    fname_match = fname_pattern.search(text) 
+    fname_match = fname_pattern.search(text)
     dob_match = dob_pattern.search(text)
 
     if name_match:
         name = name_match.group(1)
     else:
-        name = "Name not found"
+        name = "Not found!"
 
     if pan_match:
         pan = pan_match.group(1)
     else:
-        pan = "PAN not found"
+        pan = "Not found!"
 
     if fname_match:
         fname = fname_match.group(1)
     else:
-        fname = "Father's name not found"
+        fname = "Not found!"
 
     if dob_match:
         dob = dob_match.group(1)
     else:
-        dob = "Date of birth not found"
+        dob = "Not found!"
 
+    generate_pdf({"Name": name, "Father's name": fname,
+                 "Date of Birth": dob, "PAN card number": pan})
 
-
-    st.write("Name:", name)
-    st.write("Father's name:", fname)
-    st.write("Date of Birth:", dob)
-    st.write("PAN card number:", pan)
-
+    # st.write("Name:", name)
+    # st.write("Father's name:", fname)
+    # st.write("Date of Birth:", dob)
+    # st.write("PAN card number:", pan)
 
 
 def process_passport(threshold):
-     # Apply morphological operations
+    # Apply morphological operations
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
     dilate = cv2.dilate(threshold, kernel, iterations=1)
     erode = cv2.erode(dilate, kernel, iterations=1)
@@ -193,7 +225,6 @@ def process_passport(threshold):
     lines = text.split('\n')
     st.write(lines)
 
-    
     # Parse the extracted text to find the name and PAN card number using regular expressions
     passportno_pattern = re.compile(
         r"Passport\s+No\.\s+(\w\d{7})", re.IGNORECASE)
@@ -220,8 +251,6 @@ def process_passport(threshold):
     address_pattern = re.compile(
         r"Address\s*((?:.+\n)+?.*INDIA)", re.IGNORECASE)
 
-
-
     # Search for matches using the regular expressions
     passportno_match = passportno_pattern.search(text)
     surname_match = surname_pattern.search(text)
@@ -236,70 +265,65 @@ def process_passport(threshold):
     nom_match = nom_pattern.search(text)
     address_match = address_pattern.search(text)
 
-
-
-
-
     if passportno_match:
         passportno = passportno_match.group(1)
     else:
-        passportno = "Passport number not found"
-    
+        passportno = "Not found!"
+
     if surname_match:
         surname = surname_match.group(1)
     else:
-        surname = "Surname not found"
+        surname = "Not found!"
 
     if name_match:
         name = name_match.group(1)
     else:
-        name = "Name not found"
+        name = "Not found!"
 
     if sex_match:
         sex = sex_match.group(1)
     else:
-        sex = "Sex not found"
+        sex = "Not found!"
 
     if dob_match:
         dob = dob_match.group(1)
     else:
-        dob = "Date of birth not found"
+        dob = "Not found!"
 
     if pob_match:
         pob = pob_match.group(1)
     else:
-        pob = "Place of birth not found"
+        pob = "Not found!"
 
     if poi_match:
         poi = poi_match.group(1)
     else:
-        poi = "Place of issue not found"
+        poi = "Not found!"
 
     if doi_match:
         doi = doi_match.group(1)
     else:
-        poi = "Date of issue not found"
+        poi = "Not found!"
 
     if doe_match:
         doe = doe_match.group(1)
     else:
-        doe = "Date of Expiry not found"
+        doe = "Not found!"
 
     if nof_match:
         nof = nof_match.group(1)
     else:
-        nof = "Name of Father not found"
+        nof = "Not found!"
 
     if nom_match:
         nom = nom_match.group(1)
     else:
-        nom = "Name of Mother not found"
+        nom = "Not found!"
 
     if address_match:
         address = address_match.group(1)
     else:
-        address = "Address not found"
-    
+        address = "Not found!"
 
     st.write("Passport number:", passportno)
     st.write("Surname:", surname)
@@ -313,6 +337,7 @@ def process_passport(threshold):
     st.write("Name of Father:", nof)
     st.write("Name of Mother:", nom)
     st.write("Address:", address)
+
 
 if uploaded_file:
 
@@ -329,8 +354,9 @@ if uploaded_file:
         else:
 
             with io.BytesIO(uploaded_file.read()) as pdf_file:
-                images = convert_from_bytes(pdf_file.read(), poppler_path=poppler_path)
-                #images = convert_from_bytes(pdf_file.read())
+                images = convert_from_bytes(
+                    pdf_file.read(), poppler_path=poppler_path)
+                # images = convert_from_bytes(pdf_file.read())
 
             for i, img in enumerate(images):
                 # st.image(img, caption=f'PDF page {i+1}')
